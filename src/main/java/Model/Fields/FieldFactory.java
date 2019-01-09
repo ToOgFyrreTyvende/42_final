829785edd0581b9b8ca453e708291d8e979a52cf
Nicklas <nirada@live.dk>
2019-01-09 16:32:16 +0100

    Ændret farver til RGB på felter

--- src/main/java/Model/Fields/FieldFactory.java
+++ src/main/java/Model/Fields/FieldFactory.java
@@ -16,7 +16,7 @@ public class FieldFactory {
 
                 new FreeParkingField("Betal indkomstskat", "10% eller kr. 4000", "Betal plz"), // PLZ CHANGE THIS FIELD
 
-                new PropertyField("SFL", "Pris: kr. 4000", "SFL Færgen", 4000, Color.gray),
+                new PropertyField("SFL", "Pris: kr. 4000", "SFL Færgen", 4000, new Color(57,101,221)),
 
                 new PropertyField("Roskildevej", "Pris: kr. 2000", "Roskildevej", 2000, Color.pink),
 
@@ -27,39 +27,39 @@ public class FieldFactory {
 
                 new JailField("Fængsel", "På besøg", "Enten er du endt i fængsel, ellers er du på besøg."),
 
-                new PropertyField("Frederiksberg Allé", "Pris: kr. 2800", "Frederiksberg Allé", 2800, Color.green),
+                new PropertyField("Frederiksberg Allé", "Pris: kr. 2800", "Frederiksberg Allé", 2800, new Color(111,209,111)),
 
-                new PropertyField("Tuborg", "Pris: kr. 3000", "Tuborg bryggeriet", 3000, Color.gray),
+                new PropertyField("Tuborg", "Pris: kr. 3000", "Tuborg bryggeriet", 3000, new Color(68,135,32)),
 
-                new PropertyField("Bülowsvej", "Pris: kr. 2800", "Bülowsvej", 2800, Color.green),
-                new PropertyField("Gl. Kongevej", "Pris: kr. 3200", "Gl. Kongevej", 3200, Color.green),
+                new PropertyField("Bülowsvej", "Pris: kr. 2800", "Bülowsvej", 2800, new Color(111,209,111)),
+                new PropertyField("Gl. Kongevej", "Pris: kr. 3200", "Gl. Kongevej", 3200, new Color(111,209,111)),
 
-                new PropertyField("DSB", "Pris: kr. 4000", "DSB Færgen", 4000, Color.darkGray),
+                new PropertyField("DSB", "Pris: kr. 4000", "DSB Færgen", 4000, new Color(57,101,221)),
 
-                new PropertyField("Bernstorffsvej", "Pris: kr. 3600", "Bernstorffsvej", 3600, Color.darkGray),
+                new PropertyField("Bernstorffsvej", "Pris: kr. 3600", "Bernstorffsvej", 3600, Color.lightGray),
 
                 new ChanceField("Chance", "Chance", "Tag et chancekort"),
 
-                new PropertyField("Hellerupvej", "Pris: kr. 3600", "Hellerupvej", 3600, Color.darkGray),
-                new PropertyField("Strandvej", "Pris: kr. 4000", "Strandvej", 4000, Color.darkGray),
+                new PropertyField("Hellerupvej", "Pris: kr. 3600", "Hellerupvej", 3600, Color.lightGray),
+                new PropertyField("Strandvej", "Pris: kr. 4000", "Strandvej", 4000, Color.lightGray),
 
                 new FreeParkingField("Fri", "Gratis Parkering", "Hold en fridag"),
 
-                new PropertyField("Trianglen", "Pris: kr. 4400", "Trianglen", 4400, Color.red),
+                new PropertyField("Trianglen", "Pris: kr. 4400", "Trianglen", 4400, new Color(165,16,0)),
 
                 new ChanceField("Chance", "Chance", "Tag et chancekort"),
 
-                new PropertyField("Østerbrogade", "Pris: kr. 4400", "Østerbrogade", 4400, Color.red),
-                new PropertyField("Grønningen", "Pris: kr. 4800", "Grønningen", 4800, Color.red),
+                new PropertyField("Østerbrogade", "Pris: kr. 4400", "Østerbrogade", 4400, new Color(165,16,0)),
+                new PropertyField("Grønningen", "Pris: kr. 4800", "Grønningen", 4800, new Color(165,16,0)),
 
-                new PropertyField("DFDS Seaways", "Pris: kr. 4000", "DFDS Seaways Færgen", 4000, Color.gray),
+                new PropertyField("DFDS Seaways", "Pris: kr. 4000", "DFDS Seaways Færgen", 4000, new Color(57,101,221)),
 
-                new PropertyField("Bredgade", "Pris: kr. 5200", "Bredgade", 5200, Color.lightGray),
-                new PropertyField("Kgs. Nytorv", "Pris: kr. 5200", "Kgs. Nytorv", 5200, Color.lightGray),
+                new PropertyField("Bredgade", "Pris: kr. 5200", "Bredgade", 5200, Color.white),
+                new PropertyField("Kgs. Nytorv", "Pris: kr. 5200", "Kgs. Nytorv", 5200, Color.white),
 
-                new PropertyField("Coca Cola", "Pris: kr. 3000", "Coca Cola Bryggeriet", 3000, Color.darkGray),
+                new PropertyField("Coca Cola", "Pris: kr. 3000", "Coca Cola Bryggeriet", 3000, new Color(220,43,25)),
 
-                new PropertyField("Østergade", "Pris: kr. 5500", "Østergade", 5500, Color.lightGray),
+                new PropertyField("Østergade", "Pris: kr. 5500", "Østergade", 5500, Color.white),
 
                 new ToJailField("Fængsel", "Gå til fængsel", "Øv, du skal til fængsel"),
 
@@ -70,15 +70,15 @@ public class FieldFactory {
 
                 new PropertyField("Nygade", "Pris: kr. 6400", "Nygade", 6400, Color.yellow),
 
-                new PropertyField("DSB", "Pris: kr. 4000", "DSB Færgen", 4000, Color.darkGray),
+                new PropertyField("DSB", "Pris: kr. 4000", "DSB Færgen", 4000, new Color(57,101,221)),
 
                 new ChanceField("Chance", "Chance", "Tag et chancekort"),
 
-                new PropertyField("Frederiksberggade", "Pris: kr. 7000", "Frederiksberggade", 7000, Color.magenta),
+                new PropertyField("Frederiksberggade", "Pris: kr. 7000", "Frederiksberggade", 7000, new Color(134,4,190)),
 
                 new FreeParkingField("Ekstraordinær statsskat", "betal kr. 2000", "Ekstraordinær statsskat"), // PLZ CHANGE THIS FIELD
 
-                new PropertyField("Rådhuspladsen", "Pris: kr. 8000", "Rådhuspladsen", 8000, Color.magenta)
+                new PropertyField("Rådhuspladsen", "Pris: kr. 8000", "Rådhuspladsen", 8000, new Color(134,4,190))
         };
     }
 }
