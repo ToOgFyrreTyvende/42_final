350f28b751c501d81a09fa5b9ba0a85db772104b
Ahmad Sattar <thehabbos007@gmail.com>
2019-01-09 15:12:46 +0100

    Added small changes

--- src/main/java/Controller/JailController.java
+++ src/main/java/Controller/JailController.java
@@ -4,8 +4,39 @@ import Model.Global;
 
 public class JailController {
     public final String[] JailActions = new String[]{
-            "Betal"  + Global.JAIL_PRICE,
+            "Betal 1000",
             "Rul terning",
             "Brug frikort"
     };
+
+    public void handleActions(String action){
+        switch(action){
+            case "Betal 1000":
+                payBail();
+                break;
+            case "Rul terning":
+                feelingLucky();
+                break;
+            case "Brug frikort":
+                bailCard();
+                break;
+            default:
+            // Indsæt auktion funktionalitet?
+                break;
+            }
+    }
+
+
+    public void payBail(){
+
+    }
+
+    public void feelingLucky(){
+
+    }
+
+    public void bailCard(){
+
+    }
+
 }
---
+++ src/test/java/Controller/PropertyControllerTest.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyControllerTest {
+
+    PropertyController pc = new PropertyController();
+
+    @Test
+    void InitalMenuTest(){
+        // Test the initial menu
+    }
+
+}
