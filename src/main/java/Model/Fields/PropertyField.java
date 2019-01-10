f60c024a57a06deaa0ec8016a4fac12d99ffd153
Nicklas <nirada@live.dk>
2019-01-10 10:08:22 +0100

    ændret M til Kr.

--- src/main/java/Model/Fields/PropertyField.java
+++ src/main/java/Model/Fields/PropertyField.java
@@ -50,11 +50,11 @@ public class PropertyField extends Field {
         }else{
             player.setLastAction(player.getLastAction() + "\n - Har købt " +
                     this.getName() + " for " +
-                    this.getPrice() + "M");
+                    this.getPrice() + "Kr.");
 
             System.out.println("[INFO] " + player.getName() + " Har købt " +
                     this.getName() + " for " +
-                    this.getPrice()+ "M");
+                    this.getPrice()+ "Kr.");
 
             buyField(player);
         }
@@ -75,11 +75,11 @@ public class PropertyField extends Field {
 
     private void payToPlayerLogic(Player player) {
         player.setLastAction(player.getLastAction() + "\n - Har betalt " +
-                this.getPrice() + "M til " +
+                this.getPrice() + "Kr. til " +
                 this.getOwner().getName());
 
         System.out.println("[INFO] " + player.getName() + " Har betalt " +
-                this.getPrice() + "M til " +
+                this.getPrice() + "Kr. til " +
                 this.getOwner().getName());
         payToPlayerField(player);
     }
